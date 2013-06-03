12f5bef5df51784ec0527aca440a3106359483d6
kenshin54 <himurakenshin54@gmail.com>
2013-06-04 01:04:03 +0800

    Add Gruntfile.js to combine popline js files.

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> v<%= pkg.version %> | author: <%= pkg.author %> */\n'
+      },
+      target: {
+        files: {
+          'build/jquery.<%= pkg.name %>.min.js': ['scripts/jquery.<%= pkg.name %>.js', 'scripts/plugins/jquery.popline.blockformat.js', 'scripts/plugins/jquery.popline.decoration.js', 'scripts/plugins/jquery.popline.justify.js', 'scripts/plugins/jquery.popline.link.js', 'scripts/plugins/jquery.popline.list.js', 'scripts/plugins/jquery.popline.social.js']
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['uglify']);
+
+};
