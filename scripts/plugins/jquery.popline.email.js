f6a30de3fd2a5e5f8a785ce75ec49a02ebedbe9c
kenshin54 <himurakenshin54@gmail.com>
2013-08-22 13:55:48 +0800

    Add feature that email will be sensed.

---
+++ scripts/plugins/jquery.popline.email.js
@@ -0,0 +1,42 @@
+/*
+  jquery.popline.email.js 0.0.1
+
+  Version: 0.0.1
+  Updated: Aug 22th, 2013
+
+  (c) 2013 by kenshin54
+*/
+;(function($) {
+
+  var pattern = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i;
+
+  var selectionIsEmail = function() {
+    var result = false;
+    var selection = window.getSelection();
+    if (pattern.test(selection.toString())) {
+      result = true;
+    }
+    return result;
+  }
+
+  $.popline.addButton({
+    email: {
+      text: "@",
+      mode: "view",
+      beforeShow: function(popline) {
+        if (!selectionIsEmail()) {
+          this.hide();
+        }
+      },
+      action: function(event) {
+        var $emailLink = $("<a></a>").attr("id", "popline-email-link").attr("href", "mailto:" + window.getSelection().toString());
+        $emailLink.click(function(event){
+          event.stopPropagation();
+        });
+        $(this).find(".btn").after($emailLink);
+        $emailLink[0].click();
+        $emailLink.remove();
+      }
+    }
+  });
+})(jQuery);
