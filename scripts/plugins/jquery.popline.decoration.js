16cfe6b45de3f0fe5854234d0ee4038a9219105d
huwence <huwence@gmail.com>
2013-09-27 19:26:27 +0800

    1. Added modify font color plugin by huwence. 2. Fixed an issue where the position of popline bar was incorrect while srolling page

--- scripts/plugins/jquery.popline.decoration.js
+++ scripts/plugins/jquery.popline.decoration.js
@@ -8,6 +8,34 @@
 */
 ;(function($) {
 
+  var colors = [
+      'rgba(189, 4, 0, 1)',
+      'rgba(255, 0, 0, 1)',
+      'rgba(255, 204, 0, 1)',
+      'rgba(255, 255, 0, 1)',
+      'rgba(156, 190, 90, 1)',
+      'rgba(0, 174, 82, 1)',
+      'rgba(7, 168, 236, 1)',
+      'rgba(0, 36, 99, 1)',
+      'rgba(115, 73, 165, 1)'
+  ];
+
+  var getColorButtons = function (){
+      var buttons = {};
+
+      $(colors).each(function (index, color) {
+        buttons['color' + index] = {
+            bg_color: color,
+            text: '&nbsp',
+            action: function (event) {
+                document.execCommand('BackColor', "false", $(this).css('background-color'));
+            }
+        }
+      });
+
+      return buttons;
+  }
+
   $.popline.addButton({
     bold: {
       iconClass: "icon-bold",
@@ -39,6 +67,12 @@
       action: function(event) {
         document.execCommand("underline");
       }
+    },
+
+    color: {
+      iconClass: "icon-font",
+      mode: "edit",
+      buttons: getColorButtons()
     }
   });
 })(jQuery);
