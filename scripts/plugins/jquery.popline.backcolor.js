3d227c63372cdfbb2a04327b04a72bb7a72c7156
kenshin54 <himurakenshin54@gmail.com>
2013-10-20 22:30:52 +0800

    Fix Issue #21.

--- scripts/plugins/jquery.popline.backcolor.js
+++ scripts/plugins/jquery.popline.backcolor.js
@@ -9,30 +9,48 @@
 ;(function($) {
 
   var colors = [
-      'rgba(255, 0, 0, 1)',
-      'rgba(255, 255, 0, 1)',
-      'rgba(156, 190, 90, 1)',
-      'rgba(0, 174, 82, 1)',
-      'rgba(7, 168, 236, 1)',
-      'rgba(0, 36, 99, 1)',
-      'rgba(115, 73, 165, 1)',
-      'rgba(0, 0, 0, 1)'
+    '#FF0000',
+    '#FFFF00',
+    '#9CBE5A',
+    '#00AE52',
+    '#07A8EC',
+    '#002463',
+    '#7349A5',
+    '#000000'
   ];
 
+  function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+  }
+
+  function colorToHex(color) {
+    if (color.substr(0, 1) === '#') {
+      return color;
+    }
+    var digits = /(.*?)rgb\((\d+), (\d+), (\d+)\)/.exec(color);
+    
+    var red = parseInt(digits[2]);
+    var green = parseInt(digits[3]);
+    var blue = parseInt(digits[4]);
+    
+    return '#' + componentToHex(red) + componentToHex(green) + componentToHex(blue);
+  };
+
   var getColorButtons = function (){
-      var buttons = {};
+    var buttons = {};
 
-      $(colors).each(function (index, color) {
-        buttons['color' + index] = {
-            bgColor: color,
-            text: '&nbsp',
-            action: function (event) {
-                document.execCommand('ForeColor', "false", $(this).css('background-color'));
-            }
+    $(colors).each(function (index, color) {
+      buttons['color' + index] = {
+        bgColor: color,
+        text: '&nbsp',
+        action: function (event) {
+          document.execCommand('ForeColor', false, colorToHex($(this).css('background-color')));
         }
-      });
+      }
+    });
 
-      return buttons;
+    return buttons;
   }
 
   $.popline.addButton({
