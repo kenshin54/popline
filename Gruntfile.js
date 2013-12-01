8dcf46d83412903eb3c070f182ab608a43a656c2
Michael Kay <kay.mikey@gmail.com>
2013-12-01 19:21:42 +0000

    Added ability to run Jasmine tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,11 +11,32 @@ module.exports = function(grunt) {
           'build/jquery.<%= pkg.name %>.min.js': ['scripts/jquery.<%= pkg.name %>.js', 'scripts/plugins/jquery.popline.link.js', 'scripts/plugins/jquery.popline.blockquote.js', 'scripts/plugins/jquery.popline.decoration.js', 'scripts/plugins/jquery.popline.list.js', 'scripts/plugins/jquery.popline.justify.js', 'scripts/plugins/jquery.popline.blockformat.js', 'scripts/plugins/jquery.popline.social.js', 'scripts/plugins/jquery.popline.email.js', "scripts/plugins/jquery.popline.backcolor.js"]
         }
       }
+    },
+    jasmine: {
+      suite: {
+          src: 'scripts/**/*.js',
+          options: {
+              specs: 'spec/*.spec.js',
+              helpers: 'spec/helpers/*.js',
+              junit: {
+                  path: "reports/jasmine/",
+                  consolidate: true
+              },
+              keepRunner: true,
+              template: require('grunt-template-jasmine-istanbul'),
+              templateOptions: {
+                  coverage: 'reports/jasmine/coverage.json',
+                  report: 'coverage'
+              }
+          }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
 
   grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('test', ['jasmine']);
 
 };
