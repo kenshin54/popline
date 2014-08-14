72a7d96f6141b98cfae3c72551ff35659ff27607
kenshin54 <himurakenshin54@gmail.com>
2014-08-15 00:49:58 +0800

    Remove redundant empty paragraph tag under IE family.

--- scripts/plugins/jquery.popline.justify.js
+++ scripts/plugins/jquery.popline.justify.js
@@ -7,6 +7,18 @@
   (c) 2014 by kenshin54
 */
 ;(function($) {
+
+  var removeRedundantParagraphTag = function(popline, align) {
+    if ($.popline.utils.browser.ie) {
+      $paragraphs = popline.target.find("p[align=" + align + "]");
+      $paragraphs.each(function(i, obj){
+        if (obj.childNodes.length === 1 && obj.childNodes[0].nodeType === 3 && !/\S/.test(obj.childNodes[0].nodeValue)) {
+          $(obj).remove();
+        }
+      })
+    }
+  }
+
   $.popline.addButton({
     justify: {
       iconClass: "fa fa-align-justify",
@@ -14,22 +26,25 @@
       buttons: {
         justifyLeft: {
           iconClass: "fa fa-align-left",
-          action: function(event) {
+          action: function(event, popline) {
             document.execCommand("JustifyLeft");
+            removeRedundantParagraphTag(popline, "left");
           }
         },
 
         justifyCenter: {
           iconClass: "fa fa-align-center",
-          action: function(event) {
+          action: function(event, popline) {
             document.execCommand("JustifyCenter");
+            removeRedundantParagraphTag(popline, "center");
           }
         },
 
         justifyRight: {
           iconClass: "fa fa-align-right",
-          action: function(event) {
+          action: function(event, popline) {
             document.execCommand("JustifyRight");
+            removeRedundantParagraphTag(popline, "right");
           }
         },
 
