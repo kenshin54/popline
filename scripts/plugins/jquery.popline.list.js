c0f62eacb8b6cbdcdb4b3b83d72de6b97c265b48
kenshin54 <himurakenshin54@gmail.com>
2014-09-06 03:14:08 +0800

    Fix ol/ul bug under firefox.

--- scripts/plugins/jquery.popline.list.js
+++ scripts/plugins/jquery.popline.list.js
@@ -8,12 +8,39 @@
 */
 ;(function($) {
 
+  var firefoxCleanupCheck = function(tag) {
+    var focusNode = $.popline.utils.selection().focusNode();
+    var node = $.popline.utils.findNodeWithTags(focusNode, tag);
+    return node ? true : false;
+  }
+
+  var firefoxCleanup = function(addPTag) {
+    $.popline.current.target.find("br[type=_moz]").parent().remove();
+    if (addPTag) {
+      document.execCommand("formatblock", false, "P");
+      var selection = $.popline.utils.selection().obj();
+      if (selection.anchorNode && (node = selection.anchorNode.parentNode.previousSibling) && node.tagName === "BR") {
+        node.remove();
+      }
+      if (selection.focusNode && (node = selection.focusNode.parentNode.nextSibling) && node.tagName === "BR") {
+        node.remove();
+      }
+      addPTag = null;
+    }
+  }
+
   $.popline.addButton({
     orderedList: {
       iconClass: "fa fa-list-ol",
       mode: "edit",
       action: function(event) {
+        if ($.popline.utils.browser.firefox) {
+          var addPTag = firefoxCleanupCheck('OL');
+        }
         document.execCommand("InsertOrderedList");
+        if ($.popline.utils.browser.firefox) {
+          firefoxCleanup(addPTag);
+        }
       }
     },
 
@@ -21,7 +48,13 @@
       iconClass: "fa fa-list-ul",
       mode: "edit",
       action: function(event) {
+        if ($.popline.utils.browser.firefox) {
+          var addPTag = firefoxCleanupCheck('UL');
+        }
         document.execCommand("InsertUnorderedList");
+        if ($.popline.utils.browser.firefox) {
+          firefoxCleanup(addPTag);
+        }
       }
     }
   });
